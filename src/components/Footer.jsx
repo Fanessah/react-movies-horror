39f8ba5b6048dcae72ae889438674da24903f51c
Fanessa Hilaire <fan@Fanessas-MacBook-Air.local>
2022-10-27 16:17:10 -0400

    horror movie site

---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer(){
+    return (
+        <footer>
+            <p>&copy; 2022 Fanessa Hilaire. Source: <a href="https://sampleapis.com/api-list/movies" >
+                sampleapis</a></p>
+        </footer>
+    )
+}
